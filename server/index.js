a31132505665b8509ab22f2c0bea5dba523ab089
Forace Avant <themattavant@gmail.com>
2020-11-16 20:22:41 -0700

    Fixed CORS issue by switching order on app.use functions

--- server/index.js
+++ server/index.js
@@ -8,11 +8,13 @@ import postRoutes from './routes/posts.js'
 //Initialization of express app
 const app = express();
 
-app.use('/posts', postRoutes);
+
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 //Connection to MongoDB Cluster
 const CONNECTION_URL = 'mongodb+srv://foraceavant:foraceavant123@cluster0.apdb7.mongodb.net/<dbname>?retryWrites=true&w=majority';
