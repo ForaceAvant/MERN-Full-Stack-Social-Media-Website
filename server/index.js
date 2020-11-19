40cd8d1226905f775950c7ee6636ccc99a7380f2
Forace Avant <themattavant@gmail.com>
2020-11-18 19:00:03 -0700

    final production changes

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js'
 
 //Initialization of express app
 const app = express();
-
+dotenv.config();
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
@@ -16,11 +16,14 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Welcome to Memories API');
+});
+
 //Connection to MongoDB Cluster
-const CONNECTION_URL = 'mongodb+srv://foraceavant:foraceavant123@cluster0.apdb7.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
