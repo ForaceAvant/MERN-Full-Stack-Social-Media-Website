046969d349b8a48b653b6c0fb87feb51b9df9caf
Forace Avant <themattavant@gmail.com>
2020-11-18 17:38:41 -0700

    Added like post functionality to frontend

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -7,7 +7,7 @@ import moment from 'moment';
 import useStyles from './styles';
 import { useDispatch } from 'react-redux';
 
-import { deletePost } from '../../../actions/posts'
+import { deletePost, likePost } from '../../../actions/posts'
 
 
 const Post = ({ post, setCurrentId }) => {
@@ -34,10 +34,9 @@ const Post = ({ post, setCurrentId }) => {
                 <Typography className={classes.title} variant="h5" gutterBottom> {post.message} </Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" onClick={() => {}} >
+                <Button size="small" color="primary" onClick={() => {dispatch(likePost(post._id))}} >
                     <ThumbUpAltIcon fontSize="small"/>
-                    Like
-                    {post.likeCount}
+                    Like {post.likeCount}
                 </Button>
                 <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))} >
                     <DeleteIcon fontSize="small"/>
